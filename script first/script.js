611e5dda96a85bf17e23520e081f37743cc9a0c1
reddy887 <avulareddybabu@gmail.com>
2025-09-13 22:22:20 +0530

    commenting

--- script first/script.js
+++ script first/script.js
@@ -1,14 +1,14 @@
-// alert("reddy");
-// console.log("reddy");
-// let name;
-// name = "reddybabu";
-// console.log(typeof name);
-// console.warn("enter your age");
-// console.error("you did not do");
-// let last_name = prompt("enter your name", "reddy");
-// console.log(last_name);
-// let num = 10000000000000000000n;
-// console.log(typeof num);
+/*alert("reddy");
+console.log("reddy");
+let name;
+name = "reddybabu";
+console.log(typeof name);
+console.warn("enter your age");
+console.error("you did not do");
+let last_name = prompt("enter your name", "reddy");
+console.log(last_name);
+let num = 10000000000000000000n;
+console.log(typeof num);
 let details = [34, 56, 78, 89];
 console.log(details);
 console.log(typeof details);
@@ -20,4 +20,4 @@ function ba() {
   console.log(typeof re);
   console.log(re.name, re.age);
 }
-ba();
+ba();*/
