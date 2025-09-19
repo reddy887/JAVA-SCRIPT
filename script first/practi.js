7901bd52a5a5f3d7718e39cc0a0dc8ca35c98c8a
reddy887 <avulareddybabu@gmail.com>
2025-09-19 07:09:06 +0530

    arrays and objects

--- script first/practi.js
+++ script first/practi.js
@@ -1,6 +1,23 @@
-alert("reddybabu");
-let arr = "reddy,babu,hari";
-let ne = arr.split(",");
-console.log(ne);
-let ne1 = ne.join(",");
-console.log(ne1);
+// alert("reddybabu");
+// let arr = "reddy,babu,hari";
+// let ne = arr.split(",");
+// console.log(ne);
+// let ne1 = ne.join(",");
+// console.log(ne1);
+// let arr = [
+//   ["reddy", 34, "babu"],
+//   [34, "giribabu", 45],
+// ];
+// console.log(arr[1][0]);
+let obj = {
+  name: "reddy",
+  age: 23,
+  group: "bca",
+  sub: function () {
+    let a = 20;
+    let b = 34;
+    console.log(a + b);
+  },
+};
+obj.sub();
+console.log(obj.name);
