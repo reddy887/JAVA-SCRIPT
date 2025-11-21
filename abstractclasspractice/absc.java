ed366fc5050b55a4b0fb95aff904464a2efc2cd0
reddy887 <avulareddybabu@gmail.com>
2025-11-21 10:23:59 +0530

    practice

--- abstractclasspractice/absc.java
+++
@@ -1,7 +0,0 @@
-package abstractclasspractice;
-
-public  interface absc{
-		public void copy();
-		public abstract void paste();
-		public  abstract void cut();
-}
--- abstractclasspractice/mainmethid.java
+++
@@ -1,8 +0,0 @@
-package abstractclasspractice;
-
-public class mainmethid {
-    public static void main(String args[]){
-    hp h=new hp();
-    h.copy();
-    }
-}
