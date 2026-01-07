81497ac1476967371e16e3947c8f0fa9148d155d
reddy887 <avulareddybabu@gmail.com>
2026-01-07 20:48:23 +0530

    practice formevents

--- formevents/jsc.js
+++ formevents/jsc.js
@@ -1,1 +1,9 @@
-console.log("reddybabu");
+const btn = document.querySelector("button");
+const forminfobox = document.getElementById("form-info");
+const msg = document.getElementById("form-msg");
+const form = document.querySelector("form");
+btn.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const formData = new FormData(form);
+  console.log(Object.fromEntries(formData.entries()));
+});
