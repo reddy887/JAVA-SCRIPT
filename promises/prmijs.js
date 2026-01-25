653e1d7f32a9899499faef48018c4f4550b7b2c2
reddy887 <avulareddybabu@gmail.com>
2026-01-25 07:45:37 +0530

    promises using

--- promises/prmijs.js
+++ promises/prmijs.js
@@ -1,15 +1,53 @@
-let promis = new Promise((resolve, reject) => {
-  let isOnline = false;
-  if (isOnline) {
-    resolve("you are in online");
-  } else {
-    reject("your are in offline");
-  }
-});
-promis
-  .then((data) => {
-    console.log(data);
-  })
-  .catch((error) => {
-    console.log(error);
+// let promis = new Promise((resolve, reject) => {
+//   let isOnline = false;
+//   if (isOnline) {
+//     resolve("you are in online");
+//   } else {
+//     reject("your are in offline");
+//   }
+// });
+// promis
+//   .then((data) => {
+//     console.log(data);
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
+let isOnline = true;
+function step1() {
+  return new Promise((sucess, reject) => {
+    setTimeout(() => {
+      if (isOnline) {
+        sucess();
+        console.log("movie selected");
+      } else {
+        reject();
+        console.log("check your internet connection");
+      }
+    }, 2000);
+  });
+}
+
+function step2() {
+  return new Promise((sucess, reject) => {
+    setTimeout(() => {
+      sucess();
+      console.log("movie seat selected");
+    }, 2000);
+  });
+}
+
+function step3() {
+  return new Promise((sucess, reject) => {
+    setTimeout(() => {
+      sucess();
+      console.log("payment sucessfull");
+    }, 1000);
+  });
+}
+step1()
+  .then(step2)
+  .then(step3)
+  .catch(() => {
+    console.log("can't resolve data");
   });
