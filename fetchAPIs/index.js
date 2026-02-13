4130a1828646936005fab4e898a81cff6e8f5f3f
reddy887 <avulareddybabu@gmail.com>
2026-02-13 06:45:43 +0530

    practice

--- fetchAPIs/index.js
+++ fetchAPIs/index.js
@@ -1,17 +1,24 @@
 // fetch("https://saurav.tech/NewsAPI/top-headlines/category/health/in.json")
 // fetch("https://saurav.tech/NewsAPI/everything/cnn.json")
 // fetch("https://saurav.tech/NewsAPI/everything/cnn.json")
+
 fetch("https://saurav.tech/NewsAPI/top-headlines/category/health/in.json")
   .then((responce) => {
     return responce.json();
   })
   .then((data) => {
+    const container = document.getElementById("newscontainer");
     console.log(data);
-    data.articles.map((items) => {
-      console.log("Auther: " + items.author);
-      console.log("content: " + items.content);
-      console.log("description: " + items.description);
-      console.log("");
+    data.articles.map((item) => {
+      const div = document.createElement("div");
+      div.className = "news";
+      div.innerHTML = `<h>${item.title}</h>
+      <p>
+        <string>Auther:</string>${item.author ?? "not available"}
+      </p>
+      <p><<string>Description:</string>${item.description ?? "no description"}</p>
+      <small>${item.content ?? ""}</small>`;
+      container.appendChild(div);
     });
   })
   .catch((error) => {
