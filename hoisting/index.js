c092cbd36ec67f165b9c03da4a1940a1eb89e836
reddy887 <avulareddybabu@gmail.com>
2026-03-10 05:50:23 +0530

    practice

---
+++ hoisting/index.js
@@ -0,0 +1,21 @@
+// let a;
+// a = 23;
+// console.log(a);
+
+// console.log(g);
+// let g = 34;
+// console.log(g);
+
+// funk();
+
+// function funk() {
+//   console.log("the hoisting");
+// }
+// console.log(hoi); //it seem look like a variable the output:undefined
+var hoi = () => {
+  console.log("reddy");
+};
+// the accessing is hoi()
+hoi();
+
+// the hoisting happened in only in var but not in let &const
