0c95fd68d2037306df7d65c662fa528f61729e6f
reddy887 <avulareddybabu@gmail.com>
2026-03-12 05:50:06 +0530

    practiceing the named export & import

---
+++ export&import/index.js
@@ -0,0 +1,12 @@
+// import { add as sum, sub, mul } from "./script.js";
+// console.log("reddy");
+// console.log(sum(23, 34));
+// console.log(sub(34, 56));
+// console.log(mul(2, 5));
+
+import * as script from "./script.js";
+
+console.log("reddy");
+console.log(script.add(23, 34));
+console.log(script.sub(34, 56));
+console.log(script.mul(2, 5));
