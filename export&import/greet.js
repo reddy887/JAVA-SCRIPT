79ea825f0aba9f97be02171d35280cb24b744edb
reddy887 <avulareddybabu@gmail.com>
2026-03-12 06:10:49 +0530

    export and imports practice

---
+++ export&import/greet.js
@@ -0,0 +1,4 @@
+export default function greet(name) {
+  console.log(`hello,${name}`);
+}
+// export default greet;
--- export&import/index.js
+++ export&import/index.js
@@ -5,8 +5,10 @@
 // console.log(mul(2, 5));
 
 import * as script from "./script.js";
+import greet from "./greet.js";
 
 console.log("reddy");
 console.log(script.add(23, 34));
 console.log(script.sub(34, 56));
 console.log(script.mul(2, 5));
+greet("reddye");
